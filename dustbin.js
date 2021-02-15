77ea57d8040d0cf4533b3596801a829016ed08b9
spandanbasu2009 <74770709+spandanbasu2009@users.noreply.github.com>
2021-02-15 20:56:08 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,26 @@
+class Dustbin{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image = loadImage("dustbingreen.png");
+    }
+    display(){
+        var dustbinpos = this.body.position;
+        var dustbinangle = this.body.angle;
+
+        push();
+        translate(dustbinpos.x,dustbinpos.y);
+        //rotate(dustbinangle);
+        //imageMode(CENTER);
+        strokeWeight(3);
+        fill(255,0,255);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var world;
+var engine;
+var paper,dustbinPart1,dustbinPart2,dustbinPart3,ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paper = new Paper(100,450,70);
+	paper.shapeColor = color("green");
+	//dustbinPart1 = new Dustbin(1000,630,150,20);
+	dustbinPart2 = new Dustbin(1000,550,160,190);
+	//dustbinPart3 = new Dustbin(1000,550,155,200);
+	ground = new Ground(400,650,1700,10);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+  paper.display();
+  //dustbinPart1.display();
+  dustbinPart2.display();
+  //dustbinPart3.display();
+  ground.display();
+
+  //keyPressed();
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+	}
+}
+
+
+
